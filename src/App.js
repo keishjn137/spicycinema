e54ba03e04c2b24be6cb0d4078889f81b1e6c110
Nguyen Hoa <keishjn137@gmail.com>
2024-11-04 16:57:22 +0700

    xong trang home va dang nhap dang ky front end

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import Home from './Pages/Home';
 import Signin from './Pages/Signin';
@@ -9,34 +8,13 @@ function App() {
   return (
     <div className='App'>
       <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup/>} />
-      </Routes>
-    </Router>
-=======
-import logo from './logo.svg';
-import './App.css';
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> c369e6ab5439520f7848a135529505620a471d87
     </div>
   );
 }
