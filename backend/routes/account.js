e25b11ca6d076838a713144a77ee29eb910ea7cf
KhoaDev26092003 <khoa.ngkhoa27@gmail.com>
2024-12-08 20:41:25 +0700

    add api sign-in account

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -7,5 +7,5 @@ accountRoutes.get('/', account.GetAll);
 accountRoutes.post('/sign-up', account.SignUp);
 accountRoutes.put('/update-by-admin', account.UpdateByAdmin);
 accountRoutes.put('/update-by-user', account.UpdateByUser);
-
+accountRoutes.post('/sign-in', account.SignIn);
 export default accountRoutes;
